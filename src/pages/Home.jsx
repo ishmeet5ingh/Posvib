00f96cee124746fd07ade0dd0e308b800df898c1
ishmeet5ingh <ishmeet3j@gmail.com>
2024-06-03 14:20:03 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
-import { PostCard } from "../components";
+import { PostCard, PostCardSkeletonLoading } from "../components";
 import { useDispatch, useSelector } from "react-redux";
 import "../index.css";
 import { setPosts, deleteAllPost } from "../store/configSlice";
@@ -21,38 +21,43 @@ function Home() {
     }
   }, [authStatus]);
 
-  if (posts === null && authStatus !== "false") {
+  // if (posts === null && authStatus !== "false") {
+  //   return (
+  //     <>
+  //           {authStatus ? (
+  //             <h1 className="text-white">
+  //               <PostCardSkeletonLoading/>
+  //             </h1>
+  //           ) : (
+  //     <div className="py-8 sm:w-96 text-center min-h-screen">
+  //       <div className="flex flex-wrap">
+  //         <div className="p-2 w-full">
+  //             <h1 className="text-2xl font-bold text-white">
+  //               Login to read posts
+  //             </h1>
+  //         </div>
+  //       </div>
+  //     </div>
+  //           )}
+  //     </>
+  //   );
+  // } else {
     return (
       <>
-            {authStatus ? (
-              <h1 className="text-white">
-                <LoadingSpinner />
-              </h1>
-            ) : (
-      <div className="py-8 sm:w-96 text-center min-h-screen">
-        <div className="flex flex-wrap">
-          <div className="p-2 w-full">
-              <h1 className="text-2xl font-bold text-white">
-                Login to read posts
-              </h1>
-          </div>
-        </div>
-      </div>
-            )}
-      </>
-    );
-  } else {
-    return (
-      <>
-        {posts !== null &&
-          posts?.map((post, index) => (
+      {posts !== null
+        ? posts.map((post, index) => (
             <div key={post?.$id} className="w-full">
               <PostCard {...post} idx={index} />
             </div>
+          ))
+        : [1, 2, 3, 4].map((_, index) => (
+            <div key={index} className="w-full">
+              <PostCardSkeletonLoading />
+            </div>
           ))}
-      </>
+    </>
     );
-  }
+  // }
 }
 
 export default Home;
